69955a88d37a627d9298317b79572d19d368629e
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-18 15:05:02 -0300

    UIImageView

--- 5.UIImageView/UIImageView/ViewController.swift
+++ 5.UIImageView/UIImageView/ViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+   private let imageView: UIImageView = {
+       
+       let imageView = UIImageView()
+       imageView.contentMode = .scaleAspectFit
+       imageView.tintColor = UIColor.lightGray
+       imageView.image = UIImage(systemName: "gamecontroller.fill")
+       imageView.translatesAutoresizingMaskIntoConstraints = false
+       
+       imageView.layer.cornerRadius = 140
+       imageView.layer.borderWidth = 6
+       imageView.layer.borderColor = UIColor.green.cgColor
+       
+       imageView.layer.shadowColor = UIColor.black.cgColor
+       imageView.layer.shadowOpacity = 0.5
+       imageView.layer.shadowOffset = CGSize(width: 0, height: 3)
+       imageView.layer.shadowRadius = 5
+       
+       return imageView
+   }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(imageView)
+       
+        NSLayoutConstraint.activate([
+        
+            imageView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 280),
+            imageView.heightAnchor.constraint(equalToConstant: 280)
+            
+        ])
+        
     }
 
 
