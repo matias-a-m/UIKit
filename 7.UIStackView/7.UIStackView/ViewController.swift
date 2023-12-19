2ee9f9c7171170a6f1b0c8c75c2d7af5a1037ab8
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-19 09:38:01 -0300

    added UIStackView

---
+++ 7.UIStackView/7.UIStackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7.UIStackView
+//
+//  Created by matias on 19/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
