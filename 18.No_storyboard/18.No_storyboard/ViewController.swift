820ba7bcb75047018807889a732584e29391c637
Matias Adrián Molina <61393019+matias-a-m@users.noreply.github.com>
2023-12-27 18:27:02 -0300

    added no_storyboard project

---
+++ 18.No_storyboard/18.No_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  18.No_storyboard
+//
+//  Created by matias on 27/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
